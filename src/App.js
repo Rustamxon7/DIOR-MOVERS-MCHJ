695792931852a6e389866cce1b7483da1205e350
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-10-15 19:10:05 +0500

    Create hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
 
+import Hero from './components/hero/Hero.jsx'
+
 function App() {
   return (
     <div className="app">
       <h1>DIOR MOVERS</h1>
+      <Hero />
     </div>
   );
 }
