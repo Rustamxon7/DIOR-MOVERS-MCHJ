ab74dbe5088c76d32aaaab62443ea044df387e52
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-10-19 21:08:58 +0500

    Improve content

--- src/components/why/Why.jsx
+++ src/components/why/Why.jsx
@@ -4,15 +4,13 @@ const Why = () => {
   return (
     <div className='why-section'>
       <div className='container'>
-        <h2 className='subheading'>WHY CHOOSE US</h2>
-        <p className='heading-secondary'>
-          Our results speak for themselves
-        </p>
+        <h2 className='subheading'>YOUR BEST DECISION</h2>
+        <p className='heading-secondary'>Why choose Dior Movers</p>
       </div>
       <div className='container grid grid--3-cols'>
-        <div>Reason 1</div>
-        <div>Reason 2</div>
-        <div>Reason 3</div>
+        <div className='feature'>Reason 1</div>
+        <div className='feature'>Reason 2</div>
+        <div className='feature'>Reason 3</div>
       </div>
     </div>
   );
