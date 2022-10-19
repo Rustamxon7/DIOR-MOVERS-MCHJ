2a7d90b8545444abbacdce81fd2fb346fa7b3907
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-10-19 15:09:32 +0500

    Add heading to testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,14 @@
 import './Testimonials.scss';
 
 const Testimonials = () => {
-  return <div className='testimonials'>
-    <div>Left</div>
-    <div>Right</div>
-  </div>;
+  return (
+    <div className='testimonials'>
+      <div>
+        <h2 className='testimonials__heading'>See how our clients review about our services.</h2>
+      </div>
+      <div>Right</div>
+    </div>
+  );
 };
 
 export default Testimonials;
