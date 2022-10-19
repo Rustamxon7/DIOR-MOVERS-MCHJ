5f0bd5c2fc030d6e846a66b9963c997d602074a1
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-10-19 22:01:03 +0500

    Create cta box

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,7 +1,13 @@
-import './Cta.scss'
+import './Cta.scss';
 
 const Cta = () => {
-  return <div className='cta-section'>Cta</div>
-}
- 
+  return (
+    <div className='cta-section'>
+      <div className='container'>
+        <div className='cta'>a</div>
+      </div>
+    </div>
+  );
+};
+
 export default Cta;
