06ef66b7dd8e38cbfd1686e8b896ae9439926dfc
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-10-19 15:23:15 +0500

    Add star icons to testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,20 @@
 import './Testimonials.scss';
+import starIcon from '../../assets/star.png';
 
 const Testimonials = () => {
   return (
     <div className='testimonials'>
       <div>
-        <h2 className='testimonials__heading'>See how our clients review about our services.</h2>
+        <div className='testimonials__stars'>
+          <img src={starIcon} alt='star' />
+          <img src={starIcon} alt='star' />
+          <img src={starIcon} alt='star' />
+          <img src={starIcon} alt='star' />
+          <img src={starIcon} alt='star' />
+        </div>
+        <h2 className='testimonials__heading'>
+          See how our clients review about our services.
+        </h2>
       </div>
       <div>Right</div>
     </div>
