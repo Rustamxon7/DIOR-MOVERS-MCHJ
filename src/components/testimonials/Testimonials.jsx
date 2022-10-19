a52d04e12dd78eea78d902e85aba6e77931973cb
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-10-19 15:45:00 +0500

    Update testimonial images

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,9 @@
 import './Testimonials.scss';
 import starIcon from '../../assets/star.png';
 import user1Image from '../../assets/user1.jpg';
+import user2Image from '../../assets/user2.jpg';
+import user3Image from '../../assets/user3.jpg';
+import user4Image from '../../assets/user4.jpg';
 
 const Testimonials = () => {
   return (
@@ -28,31 +31,31 @@ const Testimonials = () => {
           <p className='testimonial__name'>- Lee Sims</p>
         </div>
         <div className='testimonial'>
-          <img className='testimonial__img' src={user1Image} alt='star' />
+          <img className='testimonial__img' src={user2Image} alt='star' />
           <p className='testimonial__text'>
             Nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi
             lacus sed viverra tellus in hac habitasse platea dictumst
             vestibulum.
           </p>
-          <p className='testimonial__name'>- Lee Sims</p>
+          <p className='testimonial__name'>- Nellie Rice</p>
         </div>
         <div className='testimonial'>
-          <img className='testimonial__img' src={user1Image} alt='star' />
+          <img className='testimonial__img' src={user3Image} alt='star' />
           <p className='testimonial__text'>
             Nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi
             lacus sed viverra tellus in hac habitasse platea dictumst
             vestibulum.
           </p>
-          <p className='testimonial__name'>- Lee Sims</p>
+          <p className='testimonial__name'>- Katie Peck</p>
         </div>
         <div className='testimonial'>
-          <img className='testimonial__img' src={user1Image} alt='star' />
+          <img className='testimonial__img' src={user4Image} alt='star' />
           <p className='testimonial__text'>
             Nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi
             lacus sed viverra tellus in hac habitasse platea dictumst
             vestibulum.
           </p>
-          <p className='testimonial__name'>- Lee Sims</p>
+          <p className='testimonial__name'>- Ben Newman</p>
         </div>
       </div>
     </div>
