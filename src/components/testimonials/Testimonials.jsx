2fac43cfccaa3ebe24a87355146c0fa1de812086
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-10-19 15:34:26 +0500

    Add testimonial component

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,5 +1,6 @@
 import './Testimonials.scss';
 import starIcon from '../../assets/star.png';
+import user1Image from '../../assets/user1.jpg';
 
 const Testimonials = () => {
   return (
@@ -16,7 +17,17 @@ const Testimonials = () => {
           See how our clients review about our services.
         </h2>
       </div>
-      <div>Right</div>
+      <div>
+        <div className='testimonial'>
+          <img className='testimonial__img' src={user1Image} alt='star' />
+          <p className='testimonial__text'>
+            Nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi
+            lacus sed viverra tellus in hac habitasse platea dictumst vestibulum
+            rhoncus est pellentesque elit ullamcorper
+          </p>
+          <p className='testimonial__name'>- Lee Sims</p>
+        </div>
+      </div>
     </div>
   );
 };
