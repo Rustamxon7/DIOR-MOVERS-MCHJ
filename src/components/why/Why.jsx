4f729aa1d631e46ad2202ce9579d0e2dd01b478f
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-10-19 21:06:06 +0500

    Create why choose us section

---
+++ src/components/why/Why.jsx
@@ -0,0 +1,21 @@
+import './Why.scss';
+
+const Why = () => {
+  return (
+    <div className='why-section'>
+      <div className='container'>
+        <h2 className='subheading'>WHY CHOOSE US</h2>
+        <p className='heading-secondary'>
+          Our results speak for themselves
+        </p>
+      </div>
+      <div className='container grid grid--3-cols'>
+        <div>Reason 1</div>
+        <div>Reason 2</div>
+        <div>Reason 3</div>
+      </div>
+    </div>
+  );
+};
+
+export default Why;
