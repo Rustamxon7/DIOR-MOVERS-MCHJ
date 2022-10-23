48812ff19283ab16f775b807056bacbcac484cfd
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-10-23 11:42:10 +0500

    Complete moving page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Hero from './components/hero/Hero.jsx';
 import Services from './components/services/Services';
@@ -8,17 +9,30 @@ import Faq from './components/faq/Faq';
 import Why from './components/why/Why';
 import Cta from './components/cta/Cta';
 import Footer from './components/footer/Footer';
+import Moving from './pages/Moving';
 
 function App() {
   return (
-    <div className="app">
-      <Hero />
-      <Services />
-      <Testimonials />
-      <Why />
-      <Faq />
-      <Cta />
-      <Footer />
+    <div className='app'>
+      <Router>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                <Hero />
+                <Services />
+                <Testimonials />
+                <Why />
+                <Faq />
+                <Cta />
+                <Footer />
+              </>
+            }
+          />
+          <Route path='/moving' element={<Moving />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
