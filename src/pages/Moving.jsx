ae1cd1e796689f1178b5f0ea4acc1a24bba626c5
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-10-25 10:01:14 +0500

    Fix navigation in pages

--- src/pages/Moving.jsx
+++ src/pages/Moving.jsx
@@ -1,12 +1,17 @@
 import './Page.scss';
-import Footer from '../components/footer/Footer'
+import Footer from '../components/footer/Footer';
+
+import logo from '../assets/logo.svg';
+import uzbFlag from '../assets/uzb-flag.png';
+import usaFlag from '../assets/usa-flag.png';
+import rusFlag from '../assets/rus-flag.png';
 
 const Moving = () => {
   return (
     <>
-      <header className='header'>
+      <header className='header header--page'>
         <a href='#' className='logo'>
-          {/* <img src={logo} alt='logo__image' className='logo__image' /> */}
+          <img src={logo} alt='logo__image' className='logo__image' />
         </a>
 
         <div className='lang'>
@@ -15,30 +20,32 @@ const Moving = () => {
             aria-label='Change language'
             // onClick={() => clickLang('uz')}
           >
-            UZ
+            <img src={uzbFlag} alt='' />
           </button>
           <button
             className='lang__btn'
             aria-label='Change language'
             // onClick={() => clickLang('ru')}
           >
-            RU
+            <img src={rusFlag} alt='' />
           </button>
           <button
             className='lang__btn'
             aria-label='Change language'
             // onClick={() => clickLang('en')}
           >
-            EN
+            <img src={usaFlag} alt='' />
           </button>
         </div>
         <nav className='nav'>
           <ul className='nav__list'>
-            {/* <li className='nav__link'>{t('Nav.Home')}</li> */}
-            {/* <li className='nav__link'>{t('Nav.About')}</li> */}
-            {/* <li className='nav__link'>{t('Nav.Services')}</li> */}
-            {/* <li className='nav__link'>{t('Nav.Services')}</li> */}
-            {/* <li className='nav__link contact'>{t('Nav.Contact')}</li> */}
+            <li className='nav__link'>Nav.Home</li>
+            <li className='nav__link'>Nav.About</li>
+            <li className='nav__link'>Nav.Services</li>
+            <li className='nav__link'>Nav.Services</li>
+            <a className='btn' href='#'>
+              Hero.btn1
+            </a>
           </ul>
         </nav>
       </header>
@@ -70,38 +77,38 @@ const Moving = () => {
             Cicero in 45 BC
           </h3>
           <p className='paragraph'>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
-          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
-          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
-          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
-          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
-          qui dolorem eum fugiat quo voluptas nulla pariatur?
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+            aliquid ex ea commodi consequatur? Quis autem vel eum iure
+            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
+            pariatur?
           </p>
           <h3 className='heading-tertiary'>1914 translation by H. Rackham</h3>
           <p className='paragraph'>
-          But I must
-          explain to you how all this mistaken idea of denouncing pleasure and
-          praising pain was born and I will give you a complete account of the
-          system, and expound the actual teachings of the great explorer of the
-          truth, the master-builder of human happiness. No one rejects,
-          dislikes, or avoids pleasure itself, because it is pleasure, but
-          because those who do not know how to pursue pleasure rationally
-          encounter consequences that are extremely painful. Nor again is there
-          anyone who loves or pursues or desires to obtain pain of itself,
-          because it is pain, but because occasionally circumstances occur in
-          which toil and pain can procure him some great pleasure. To take a
-          trivial example, which of us ever undertakes laborious physical
-          exercise, except to obtain some advantage from it? But who has any
-          right to find fault with a man who chooses to enjoy a pleasure that
-          has no annoying consequences, or one who avoids a pain that produces
-          no resultant pleasure?
+            But I must explain to you how all this mistaken idea of denouncing
+            pleasure and praising pain was born and I will give you a complete
+            account of the system, and expound the actual teachings of the great
+            explorer of the truth, the master-builder of human happiness. No one
+            rejects, dislikes, or avoids pleasure itself, because it is
+            pleasure, but because those who do not know how to pursue pleasure
+            rationally encounter consequences that are extremely painful. Nor
+            again is there anyone who loves or pursues or desires to obtain pain
+            of itself, because it is pain, but because occasionally
+            circumstances occur in which toil and pain can procure him some
+            great pleasure. To take a trivial example, which of us ever
+            undertakes laborious physical exercise, except to obtain some
+            advantage from it? But who has any right to find fault with a man
+            who chooses to enjoy a pleasure that has no annoying consequences,
+            or one who avoids a pain that produces no resultant pleasure?
           </p>
         </div>
         <Footer />
