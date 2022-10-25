4262e4832539b4128034959ce23f3784e91e4750
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-10-25 08:44:54 +0500

    Add lang icons

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,6 +4,9 @@ import './Hero.scss';
 
 import logo from '../../assets/logo.svg';
 import heroImg from '../../assets/hero-img.jpg';
+import uzbFlag from '../../assets/uzb-flag.png';
+import usaFlag from '../../assets/usa-flag.png';
+import rusFlag from '../../assets/rus-flag.png';
 
 import { IoArrowDown } from 'react-icons/io5';
 
@@ -16,61 +19,61 @@ const Hero = () => {
 
   return (
     <>
-      <header className="header">
-        <a href="#" className="logo">
-          <img src={logo} alt="logo__image" className="logo__image" />
+      <header className='header'>
+        <a href='#' className='logo'>
+          <img src={logo} alt='logo__image' className='logo__image' />
         </a>
 
-        <div className="lang">
+        <div className='lang'>
           <button
-            className="lang__btn"
-            aria-label="Change language"
+            className='lang__btn'
+            aria-label='Change language'
             onClick={() => clickLang('uz')}
           >
-            UZ
+            <img src={uzbFlag} alt='' />
           </button>
           <button
-            className="lang__btn"
-            aria-label="Change language"
+            className='lang__btn'
+            aria-label='Change language'
             onClick={() => clickLang('ru')}
           >
-            RU
+            <img src={rusFlag} alt='' />
           </button>
           <button
-            className="lang__btn"
-            aria-label="Change language"
+            className='lang__btn'
+            aria-label='Change language'
             onClick={() => clickLang('en')}
           >
-            EN
+            <img src={usaFlag} alt='' />
           </button>
         </div>
-        <nav className="nav">
-          <ul className="nav__list">
-            <li className="nav__link">{t('Nav.Home')}</li>
-            <li className="nav__link">{t('Nav.About')}</li>
-            <li className="nav__link">{t('Nav.Services')}</li>
-            <li className="nav__link">{t('Nav.Services')}</li>
-            <li className="nav__link contact">{t('Nav.Contact')}</li>
+        <nav className='nav'>
+          <ul className='nav__list'>
+            <li className='nav__link'>{t('Nav.Home')}</li>
+            <li className='nav__link'>{t('Nav.About')}</li>
+            <li className='nav__link'>{t('Nav.Services')}</li>
+            <li className='nav__link'>{t('Nav.Services')}</li>
+            <li className='nav__link contact'>{t('Nav.Contact')}</li>
           </ul>
         </nav>
       </header>
-      <div className="hero">
-        <div className="container container--hero grid grid--2-cols">
-          <div className="hero__text-box">
-            <h1 className="heading-primary">{t('Hero.title')}</h1>
-            <p className="hero__description">{t('Hero.description')}</p>
-            <div className="btns">
-              <a className="btn" href="#">
+      <div className='hero'>
+        <div className='container container--hero grid grid--2-cols'>
+          <div className='hero__text-box'>
+            <h1 className='heading-primary'>{t('Hero.title')}</h1>
+            <p className='hero__description'>{t('Hero.description')}</p>
+            <div className='btns'>
+              <a className='btn' href='#'>
                 {t('Hero.btn1')}
               </a>
-              <a className="btn btn--secondary" href="#">
+              <a className='btn btn--secondary' href='#'>
                 {t('Hero.btn2')}
                 <IoArrowDown />
               </a>
             </div>
           </div>
-          <div className="hero__img-box">
-            <img className="hero__img" src={heroImg} alt="" />
+          <div className='hero__img-box'>
+            <img className='hero__img' src={heroImg} alt='' />
           </div>
         </div>
       </div>
